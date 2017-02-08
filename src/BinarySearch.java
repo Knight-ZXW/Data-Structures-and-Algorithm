d3ea6ae1328354d90cf3c85ca62e6c61537a2dcf
zhuoxiuwu <zhuoxiuwu@diyidan.com>
2017-02-08 23:31:46 +0800

    add binatry search

---
+++ src/BinarySearch.java
@@ -0,0 +1,35 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+/**
+ * Created by zhuoxiuwu on 2017/2/8
+ */
+public class BinarySearch {
+
+    public static int rank(int key ,int []a ){
+        int lo = 0;
+        int hi = a.length -1;
+        while (lo <= hi){
+            int mid = lo + (hi - lo)/2;
+            if (key<a[mid]) hi = mid -1;
+            else if (key > a[mid]){
+                lo = mid +1;
+            } else return mid;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] whiteList = In.readInts(args[0]);
+        Arrays.sort(whiteList);
+        while (!StdIn.isEmpty()){
+            int key = StdIn.readInt();
+            if (rank(key,whiteList)<0){
+                StdOut.println(key);
+            }
+        }
+    }
+}
